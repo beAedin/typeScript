5464e56b91c18248505e4f980dea7ed20e916357
beAedin <panda01com@g.yju.ac.kr>
2022-07-17 21:24:02 +0900

    method chainng, map, filter, reduce

---
+++ src/arr_tuple/map-square.ts
@@ -0,0 +1,4 @@
+import { range } from "./range";
+
+let squres: number[] = range(1, 5 + 1).map((val: number) => val * val);
+console.log(squres);
--- src/arr_tuple/range.ts
+++ src/arr_tuple/range.ts
@@ -2,4 +2,3 @@ export const range = (from: number, to: number): number[] =>
     from < to ? [from, ...range(from + 1, to)] : [];
 
 let numbers: number[] = range(1, 9 + 1);
-console.log(numbers);
