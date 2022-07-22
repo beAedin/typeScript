b667552afdd3451e76202c1854343cd60a1503c1
beAedin <panda01com@g.yju.ac.kr>
2022-07-22 15:56:30 +0900

    filter, deep copy, reduce

---
+++ src/arr_tuple/pureSort.ts
@@ -0,0 +1,9 @@
+export const pureSort = <T>(array: readonly T[]): T[] => {
+    let deepCopied = [...array];
+    return deepCopied.sort();
+};
+
+export const puresort = <T>(array: readonly T[]): T[] => {
+    let deepCopied = [...array];
+    return deepCopied.sort();
+};
--- src/arr_tuple/reduce-multiply.ts
+++ src/arr_tuple/reduce-multiply.ts
@@ -4,4 +4,3 @@ let reduceSum: number = range(1, 10 + 1).reduce(
     (result: number, value: number) => result * value,
     1
 );
-console.log(reduceSum);
